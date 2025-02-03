c182aac4154d50c7583b553d7cab3e74b4f39ea2
Sorokin Konstantin <konstantin.69.sorokin@gmail.com>
2025-02-03 20:28:21 +0300

    created a biography model with foreign key to the author

---
+++ app/models/biography.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Date, String, ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+from .base import Base
+from .mixins.id_pk import IdPkMixin
+
+if TYPE_CHECKING:
+    from .author import Author
+
+
+class Biography(IdPkMixin, Base):
+    __tablename__ = "biographies"
+
+    content: Mapped[str] = mapped_column(String, nullable=False)
+    date_of_birth: Mapped[Date | None] = mapped_column(Date, nullable=True)
+    date_of_death: Mapped[Date | None] = mapped_column(Date, nullable=True)
+    nationality: Mapped[str | None] = mapped_column(String, nullable=True)
+
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"), unique=True)
+    author: Mapped["Author"] = relationship(back_populates="biography")
