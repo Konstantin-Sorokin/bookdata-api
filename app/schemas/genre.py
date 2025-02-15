40a8d6d6581106a639a18384e238c38b8a1f4ba4
Sorokin Konstantin <konstantin.69.sorokin@gmail.com>
2025-02-15 20:59:19 +0300

    updated schema for genre

--- app/schemas/genre.py
+++ app/schemas/genre.py
@@ -4,22 +4,63 @@ from .mixins import ConfigDictMixin, IdMixin
 
 
 class GenreBase(BaseModel):
+    """
+    Схема жанра базовая
+
+    Поля:
+        name (str): название жанра на русском
+        slug (str): английское название жанра
+    """
+
     name: str
     slug: str
 
 
 class GenreRead(ConfigDictMixin, IdMixin, GenreBase):
+    """
+    Схема получения данных жанра
+
+    Поля:
+        id (int)
+        name (str): название жанра на русском
+        slug (str): английское название жанра
+    """
+
     pass
 
 
 class GenreCreate(GenreBase):
+    """
+    Схема для создания жанра
+
+    Поля:
+        name (str): название жанра на русском
+        slug (str): английское название жанра
+    """
+
     pass
 
 
 class GenreUpdate(GenreCreate):
+    """
+    Схема для полного обновления жанра
+
+    Поля:
+        name (str): название жанра на русском
+        slug (str): английское название жанра
+    """
+
     pass
 
 
 class GenrePartialUpdate(GenreCreate):
+    """
+    Схема для частичного обновления жанра
+
+    Поля:
+        name (str | None): название жанра на русском
+        slug (str | None): английское название жанра
+    """
+
     name: str | None = None
     slug: str | None = None
