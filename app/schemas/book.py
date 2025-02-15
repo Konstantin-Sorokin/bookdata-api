c116845bae753be04695c5dbe56dd7a625ca0746
Sorokin Konstantin <konstantin.69.sorokin@gmail.com>
2025-02-15 19:57:48 +0300

    updated schema for book

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -10,23 +10,94 @@ if TYPE_CHECKING:
 
 
 class BookBase(BaseModel):
+    """
+    Схема книги базовая
+
+    Поля:
+        title (str)
+    """
+
     title: str
+
+
+class BookReadMin(ConfigDictMixin, IdMixin, BookBase):
+    """
+    Схема получения минимальных данных книги
+
+    Поля:
+        id (int)
+        title (str)
+    """
+
+    pass
+
+
+class BookPreviewRead(BookReadMin):
+    """
+    Схема получения данных для превью книги
+
+    Поля:
+        id (int)
+        title (str)
+        authors (list[AuthorRead])
+    """
+
+    authors: list["AuthorRead"]
+
+
+class BookFullRead(BookPreviewRead):
+    """
+    Схема получения всех данных книги
+
+    Поля:
+        id (int)
+        title (str)
+        description (str | None)
+        publication_year (int | None)
+        authors (list[AuthorRead])
+        genres (list[GenreRead])
+    """
+
     description: str | None = None
     publication_year: int | None = None
-
-
-class BookRead(ConfigDictMixin, IdMixin, BookBase):
-    authors: list["AuthorRead"]
     genres: list["GenreRead"]
 
 
 class BookCreate(BookBase):
-    pass
+    """
+    Схема для создания книги
+
+    Поля:
+        title (str)
+        description (str | None)
+        publication_year (int | None)
+    """
+
+    description: str | None = None
+    publication_year: int | None = None
 
 
 class BookUpdate(BookCreate):
+    """
+    Схема для полного обновления книги
+
+    Поля:
+        title (str)
+        description (str | None)
+        publication_year (int | None)
+    """
+
     pass
 
 
 class BookPartialUpdate(BookCreate):
+    """
+    Схема для частичного обновления книги
+
+    Поля:
+        title (str | None)
+        description (str | None)
+        publication_year (int | None)
+    """
+
     title: str | None = None
